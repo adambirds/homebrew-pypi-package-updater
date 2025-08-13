600f584b6a4cc018e41aa71aad6597565a73f9ee
GitHub Action <action@github.com>
2025-08-13 23:40:38 +0000

    Update Homebrew formula to v0.2.15

--- Formula/pypi-package-updater.rb
+++ Formula/pypi-package-updater.rb
@@ -6,10 +6,10 @@ class PypiPackageUpdater < Formula
 
   desc "A tool to update Python package dependencies across multiple file formats"
   homepage "https://github.com/adambirds/pypi-package-updater"
-  url "https://github.com/adambirds/pypi-package-updater/archive/refs/tags/v0.2.14.tar.gz"
-  sha256 "e12d08a7ef653b080d7f5f77aa31810d052f70a6d3daf923d86f2d7ccdab2524"  # Auto-updated by CI
+  url "https://github.com/adambirds/pypi-package-updater/archive/refs/tags/v0.2.15.tar.gz"
+  sha256 "c63e3a5ee35617912faa595739fd7ea4a73542faf639222329ab3a245f79fc8e"  # Auto-updated by CI
   license "MIT"
-  version "0.2.14"  # Auto-updated by CI
+  version "0.2.15"  # Auto-updated by CI
 
   depends_on "python@3.11"
 
